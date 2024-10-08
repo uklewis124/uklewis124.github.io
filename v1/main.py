51a22d29f28fca3bdf141fe17e95b7f0d5b2427a
uklewis124 <53154528+uklewis124@users.noreply.github.com>
2024-10-08 11:51:25 +0000

    refactor: Remove unnecessary code and update index.html and main.py files

---
+++ v1/main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Yes!'
+
+if __name__ == "__main__":
+    app.run(debug=True)
